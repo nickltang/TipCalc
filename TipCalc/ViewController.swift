8b0ea0060ae396359cb08e59f8e65cf5fc7b250a
Nick <nicholasltang21@gmail.com>
2020-08-20 21:10:28 -0700

    Basic functionality of TipCalc: text field for bill amount, changing tip and total amounts based on selected tip percentage.

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get initial bill amount and calculate tips
+        let bill = Double(billTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        // Calculate tip and total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update tip and total labels
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalAmountLabel.text = String(format: "$%.2f", total)
+    }
 }
 
